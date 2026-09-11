d1560444cea19d82ef61524bf9308a4aec77b92f
Aniket Tiwari <aniket120208@gmail.com>
2026-09-12 03:27:30 +0530

    style: updated home screen UI

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,100 +9,110 @@ class HomeScreen extends StatelessWidget{
     SingleChildScrollView(child: 
     Column(
       children: [
-        Padding(padding: EdgeInsetsGeometry.only(left: 5),
-        child:
-       Row(
-        children: [
-          Padding(padding: EdgeInsetsGeometry.all(5),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Column(children: [
+        
+            Padding(padding: EdgeInsetsGeometry.only(left: 5),
+            child:
+           Row(
+            children: [
+              Padding(padding: EdgeInsetsGeometry.all(5),
+              child:
+              OutlinedButton(onPressed: (){},style: OutlinedButton.styleFrom(textStyle: TextStyle(color: Colors.white,fontSize: 15)) ,child: Text('Music'),),),
+              Padding(padding: EdgeInsetsGeometry.all(5),
+              child: 
+              OutlinedButton(onPressed: (){},style: OutlinedButton.styleFrom(textStyle: TextStyle(color: Colors.white,fontSize: 15)) , child: Text('Podcasts')),),
+              Padding(padding: EdgeInsetsGeometry.all(5),
+              child: 
+              OutlinedButton(onPressed: (){},style: OutlinedButton.styleFrom(textStyle: TextStyle(color: Colors.white,fontSize: 15)) , child: Text('JioTunes')))
+            ],
+          ),),
+          SizedBox(height: 5,),
+          Padding(padding: EdgeInsetsGeometry.only(left: 10),
           child:
-          OutlinedButton(onPressed: (){},style: OutlinedButton.styleFrom(textStyle: TextStyle(color: Colors.white,fontSize: 15)) ,child: Text('Music'),),),
-          Padding(padding: EdgeInsetsGeometry.all(5),
-          child: 
-          OutlinedButton(onPressed: (){},style: OutlinedButton.styleFrom(textStyle: TextStyle(color: Colors.white,fontSize: 15)) , child: Text('Podcasts')),),
-          Padding(padding: EdgeInsetsGeometry.all(5),
-          child: 
-          OutlinedButton(onPressed: (){},style: OutlinedButton.styleFrom(textStyle: TextStyle(color: Colors.white,fontSize: 15)) , child: Text('JioTunes')))
-        ],
-      ),),
-      SizedBox(height: 5,),
-      Padding(padding: EdgeInsetsGeometry.only(left: 10),
-      child:
-      Row(
-      children: [
-      Text('Your Usuals',style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold),),],),),
-      SizedBox(height: 15,),
-      Padding(padding: EdgeInsetsGeometry.only(left: 10,right: 10),
-      child: 
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-        MusicCard(),
-        MusicCard(),
-        MusicCard(),
-      ],),),
-      SizedBox(height: 15,),
-      Padding(padding: EdgeInsetsGeometry.only(left: 10,right: 10),
-      child: 
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-        MusicCard(),
-        MusicCard(),
-        MusicCard(),
-      ],),),
-      SizedBox(height: 15,),
-      Padding(padding: EdgeInsetsGeometry.only(left: 10,right: 10),
-      child: 
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-        MusicCard(),
-        MusicCard(),
-        MusicCard(),
-      ],),),
-      SizedBox(height: 20,),
-      Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Card(
-          color: const Color.fromARGB(80, 0, 0, 0),
-          child: 
-          Padding(padding: EdgeInsetsGeometry.all(5),
+          Row(
+          children: [
+          Text('Your Usuals',style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold),),],),),
+          SizedBox(height: 15,),
+          Padding(padding: EdgeInsetsGeometry.only(left: 10,right: 10),
           child: 
           Row(
-            children: [
-              Icon(Icons.music_note,color: Colors.white, size: 100,),
-              SizedBox(width: 2,),
-              Text('Unlimited JioTunes Available')
-            ],
-          ),
-          ),
-        ),
-      ),
-      SizedBox(height: 17,),
-      Padding(padding: EdgeInsetsGeometry.only(left: 10),
-      child:
-      Row(
-      children: [
-      Text("It's Party Time!",style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold),),],),),
-      SizedBox(height: 5,),
-      Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              MusicCard(),
-              MusicCard(),
-              MusicCard(),
-              MusicCard(),
-              MusicCard(),
-            ],
+            MusicCard(),
+            MusicCard(),
+            MusicCard(),
+            MusicCard(),
+            MusicCard(),
+          ],),),
+          SizedBox(height: 15,),
+          Padding(padding: EdgeInsetsGeometry.only(left: 10,right: 10),
+          child: 
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+            MusicCard(),
+            MusicCard(),
+            MusicCard(),
+            MusicCard(),
+            MusicCard(),
+          ],),),
+          SizedBox(height: 15,),
+          Padding(padding: EdgeInsetsGeometry.only(left: 10,right: 10),
+          child: 
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+            MusicCard(),
+            MusicCard(),
+            MusicCard(),
+            MusicCard(),
+            MusicCard(),
+          ],),),],),),
+          SizedBox(height: 20,),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+              color: const Color.fromARGB(80, 0, 0, 0),
+              child: 
+              Padding(padding: EdgeInsetsGeometry.all(5),
+              child: 
+              Row(
+                children: [
+                  Icon(Icons.music_note,color: Colors.white, size: 100,),
+                  SizedBox(width: 2,),
+                  Text('Unlimited JioTunes Available')
+                ],
+              ),
+              ),
+            ),
           ),
+          SizedBox(height: 17,),
+          Padding(padding: EdgeInsetsGeometry.only(left: 10),
+          child:
+          Row(
+          children: [
+          Text("It's Party Time!",style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold),),],),),
+          SizedBox(height: 5,),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  MusicCard(),
+                  MusicCard(),
+                  MusicCard(),
+                  MusicCard(),
+                  MusicCard(),
+                ],
+              ),
+            ),
+          )
+          ],
         ),
-      )
-      ],
-    ),
     );
   }
 }
